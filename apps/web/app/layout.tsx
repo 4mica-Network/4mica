44778e8fe9bcba68364040aa8f9f937da4ba0be8
moshafiei <mohsenshafieitafreshi@gmail.com>
2026-06-15 13:38:02 -0400

    feat(web): add intervar font and update all fonts

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,34 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Pacifico, Space_Grotesk } from "next/font/google";
+import globalFontsVariables from "../fonts";
 import "./globals.css";
 import { LinkConfig } from "@4mica/url";
 import GlobalNetworkBackground from "@components/GlobalNetworkBackground";
 import AppKitProvider from "@context/AppKitProvider";
 import { HOME_META_DATA } from "@seo/home";
-
-const pacifico = Pacifico({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-pacifico",
-});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-display",
-  weight: ["400", "500", "600", "700"],
-});
 
 const { base } = new LinkConfig({
   ...process.env,
@@ -54,7 +30,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning={true}>
       <body
         suppressHydrationWarning={true}
-        className={`${geistSans.variable} ${geistMono.variable} ${pacifico.variable} ${spaceGrotesk.variable} antialiased`}
+        className={`${globalFontsVariables} antialiased`}
       >
         <GlobalNetworkBackground />
         <AppKitProvider>
