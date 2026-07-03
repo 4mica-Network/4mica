d1f98ffbe0f9821cb12a3cd333c2aebbe98399fb
moshafiei <mohsenshafieitafreshi@gmail.com>
2026-07-03 17:21:29 -0400

    feat(root): remove magical numbers from styles and moved magic numbers into tailwind config

--- packages/tailwind-config/src/preset.ts
+++ packages/tailwind-config/src/preset.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 import animate from "tailwindcss-animate";
-import { borderRadius, colors, fontSize } from "./tokens";
+import { borderRadius, colors, fontSize, spacing } from "./tokens";
 
 export const preset = {
   theme: {
@@ -8,6 +8,7 @@ export const preset = {
       borderRadius,
       colors,
       fontSize,
+      spacing,
     },
   },
   plugins: [animate],
